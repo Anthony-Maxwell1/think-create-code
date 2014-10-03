b0916ffdbb66db63c690514e3d24d64fec016c7e
Jill Vogel <jill.vogel@adelaide.edu.au>
2014-10-03 10:55:11 +0930

    ADX-7 Moved the WSGIPythonPath app paths into the wsgi.py wrapper, making them relative to the running wsgi.py file. Required so that we can have multiple instances of the app run via apache.

--- gallery/wsgi.py
+++ gallery/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Append the app base path and virtualenv dirs in the python path
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../', '.virtualenv'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gallery.settings")
 
 from django.core.wsgi import get_wsgi_application
