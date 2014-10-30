90b83f7c8dde8fe0108c376ad7f129d917809858
Jill Vogel <jill.vogel@adelaide.edu.au>
2014-10-30 14:02:04 +1030

    ADX-29 Added exhibition model tests to test can_* methods and released_yet property

--- exhibitions/tests/test_models.py
+++ exhibitions/tests/test_models.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
 from django.db import IntegrityError
 from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from exhibitions.models import Exhibition, ExhibitionForm
 from gallery.tests import UserSetUp
-from artwork.models import Artwork
+from exhibitions.models import Exhibition, ExhibitionForm
 
 
-class ExhibitionTests(TestCase):
+class ExhibitionTests(UserSetUp, TestCase):
     """Exhibition model tests."""
 
     def test_str(self):
@@ -19,6 +18,115 @@ class ExhibitionTests(TestCase):
             str(exhibition),
             'New Exhibition'
         )
+
+    def test_released_yet(self):
+        now = timezone.now()
+
+        # Released yesterday
+        yesterday = Exhibition(
+            title='New Exhibition',
+            description='description goes here', 
+            released_at=now + timedelta(hours=-24))
+        self.assertTrue(yesterday.released_yet)
+
+        # Released today
+        today = Exhibition(
+            title='New Exhibition',
+            description='description goes here',
+            released_at=now)
+        self.assertTrue(today.released_yet)
+
+        # Released tomorrow
+        tomorrow = Exhibition(
+            title='New Exhibition',
+            description='description goes here',
+            released_at=now + timedelta(hours=24))
+        self.assertFalse(tomorrow.released_yet)
+
+    def test_can_see(self):
+        now = timezone.now()
+
+        # Released yesterday
+        yesterday = Exhibition(
+            title='New Exhibition',
+            description='description goes here', 
+            released_at=now + timedelta(hours=-24))
+        self.assertTrue(yesterday.can_see(self.user))
+        self.assertTrue(yesterday.can_see(self.staff_user))
+        self.assertTrue(yesterday.can_see(self.super_user))
+
+        # Released today
+        today = Exhibition(
+            title='New Exhibition',
+            description='description goes here',
+            released_at=now)
+        self.assertTrue(today.can_see(self.user))
+        self.assertTrue(today.can_see(self.staff_user))
+        self.assertTrue(today.can_see(self.super_user))
+
+        # Released today
+        tomorrow = Exhibition(
+            title='New Exhibition',
+            description='description goes here',
+            released_at=now + timedelta(hours=24))
+        self.assertFalse(tomorrow.can_see(self.user))
+        self.assertTrue(tomorrow.can_see(self.staff_user))
+        self.assertTrue(tomorrow.can_see(self.super_user))
+
+    def test_can_save(self):
+        exhibition = Exhibition(
+            author=self.super_user,
+            title='New Exhibition',
+            description='description goes here',
+            released_at=timezone.now())
+        self.assertFalse(exhibition.can_save(self.user))
+        self.assertTrue(exhibition.can_save(self.staff_user))
+        self.assertTrue(exhibition.can_save(self.super_user))
+
+    def test_can_see_queryset(self):
+        now = timezone.now()
+
+        # Released yesterday
+        yesterday = Exhibition(
+            author=self.user,
+            title='Yesterday Exhibition',
+            description='description goes here', 
+            released_at=now + timedelta(hours=-24))
+        yesterday.save()
+
+        # Released today
+        today = Exhibition(
+            author=self.user,
+            title='Today Exhibition',
+            description='description goes here',
+            released_at=now)
+        today.save()
+
+        # Released tomorrow
+        tomorrow = Exhibition(
+            author=self.user,
+            title='Tomorrow Exhibition',
+            description='description goes here',
+            released_at=now + timedelta(hours=24))
+        tomorrow.save()
+
+        student_qs = Exhibition.can_see_queryset(Exhibition.objects, self.user)
+        self.assertEqual(len(student_qs.all()), 2)
+        self.assertEqual(student_qs.all()[0].id, yesterday.id)
+        self.assertEqual(student_qs.all()[1].id, today.id)
+
+        staff_qs = Exhibition.can_see_queryset(Exhibition.objects, self.staff_user)
+        self.assertEqual(len(staff_qs.all()), 3)
+        self.assertEqual(staff_qs.all()[0].id, yesterday.id)
+        self.assertEqual(staff_qs.all()[1].id, today.id)
+        self.assertEqual(staff_qs.all()[2].id, tomorrow.id)
+
+        super_qs = Exhibition.can_see_queryset(Exhibition.objects, self.super_user)
+        self.assertEqual(len(super_qs.all()), 3)
+        self.assertEqual(super_qs.all()[0].id, yesterday.id)
+        self.assertEqual(super_qs.all()[1].id, today.id)
+        self.assertEqual(super_qs.all()[2].id, tomorrow.id)
+
 
 class ExhibitionModelFormTests(UserSetUp, TestCase):
     """model.ExhibitionForm tests."""
