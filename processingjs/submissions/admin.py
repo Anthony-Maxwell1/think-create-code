af3fa1a9c99743fef86bd5970313f4ea019d56da
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-03-25 12:57:43 +1030

    ADX-46 Allow submissions to be filtered by exhibition in the Admin

--- processingjs/submissions/admin.py
+++ processingjs/submissions/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from submissions.models import Submission
 
 class SubmissionAdmin(admin.ModelAdmin):
-    list_filter = ('submitted_by',)
+    list_filter = ('submitted_by','exhibition_id',)
 
 admin.site.register(Submission, SubmissionAdmin)
