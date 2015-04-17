429123c450ab5e2de2cf2ec6f80a147a1207643e
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-04-17 11:21:12 +0930

    ADX-115 Added /think.create.code/all, to showcase the Sandbox page

---
+++ all/content.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var content = [
+        { title: "ProcessingJS Sandbox",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/static/sandbox.html",
+          width: 800,
+          height: 1000
+        }
+    ];
+
+    CourseUnits.show(content, 'Think.Create.Code : Interactives');
+});
