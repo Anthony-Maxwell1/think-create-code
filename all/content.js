03ca013d950e10417ed6d0464e790e4daecc0d6e
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-04-17 12:03:28 +0930

    ADX-115 tweaked the sizes of the code-reorder iframes

--- all/content.js
+++ all/content.js
@@ -7,23 +7,23 @@ $(document).ready(function() {
         },
         { title: "Code Reordering : Loops and Nesting",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_3_3_7.html",
-          width: 800,
-          height: 1000
+          width: 700,
+          height: 1050
         },
         { title: "Code Reordering : Making Choices",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_1_4.html",
-          width: 800,
-          height: 1000
+          width: 750,
+          height: 700
         },
         { title: "Code Reordering : Decision Making",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_2_7.html",
-          width: 900,
-          height: 1300
+          width: 750,
+          height: 1350
         },
         { title: "Code Reordering : Shapes, Translating, Rotating, and Scaling",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_5_3_5.html",
-          width: 800,
-          height: 1000
+          width: 750,
+          height: 700
         }
     ];
 
