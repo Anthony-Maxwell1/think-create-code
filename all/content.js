259bdc55bc1e3ce63ed5d03eade67656c9e67379
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-04-17 11:54:55 +0930

    ADX-115 Added the code-reorder exercises to /think.create.code/all

--- all/content.js
+++ all/content.js
@@ -4,6 +4,26 @@ $(document).ready(function() {
           src: "https://lti-adx.adelaide.edu.au/think.create.code/static/sandbox.html",
           width: 800,
           height: 1000
+        },
+        { title: "Code Reordering : Loops and Nesting",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_3_3_7.html",
+          width: 800,
+          height: 1000
+        },
+        { title: "Code Reordering : Making Choices",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_1_4.html",
+          width: 800,
+          height: 1000
+        },
+        { title: "Code Reordering : Decision Making",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_2_7.html",
+          width: 900,
+          height: 1300
+        },
+        { title: "Code Reordering : Shapes, Translating, Rotating, and Scaling",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_5_3_5.html",
+          width: 800,
+          height: 1000
         }
     ];
 
