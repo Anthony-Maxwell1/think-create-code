7cd9fb84e6cddfe656e023d67d104b3717c9d201
Jill Vogel <jill.vogel@adelaide.edu.au>
2015-09-14 17:38:06 +0930

    ADX-179 Updated /all content to reflect iframe sizing with References footer, and to use the latest sandbox.html instead of the old one.

--- all/content.js
+++ all/content.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
     var content = [
         { title: "ProcessingJS Sandbox",
-          src: "https://lti-adx.adelaide.edu.au/think.create.code/static/sandbox.html",
+          src: "https://lti-adx.adelaide.edu.au/think.create.code/3t2015/static/sandbox.html",
           width: 800,
-          height: 1000
+          height: 800
         },
         { title: "Code Reordering : Loops and Nesting",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_3_3_7.html",
           width: 700,
-          height: 1050
+          height: 1200
         },
         { title: "Code Reordering : Making Choices",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_1_4.html",
           width: 750,
-          height: 700
+          height: 800
         },
         { title: "Code Reordering : Decision Making",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_4_2_7.html",
           width: 750,
-          height: 1350
+          height: 1550
         },
         { title: "Code Reordering : Shapes, Translating, Rotating, and Scaling",
           src: "https://lti-adx.adelaide.edu.au/think.create.code/reorder/code_reorder_5_3_5.html",
           width: 750,
-          height: 700
+          height: 850
         }
     ];
 
